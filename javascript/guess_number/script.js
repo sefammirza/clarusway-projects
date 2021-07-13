e07b14c192e87b10fc7385196c00b7cb857f8984
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-07-13 23:45:10 +0300

    guess number

--- javascript/guess_number/script.js
+++ javascript/guess_number/script.js
@@ -1,14 +1,14 @@
- let btn = document.getElementById('btn');
- let output = document.getElementById('outputtext');
- let outputCount = document.getElementById('outputcount')
+ const btn = document.getElementById('btn');
+ const output = document.getElementById('outputtext');
+ const outputCount = document.getElementById('outputcount')
 
-var county = 1
+let county = 1
 
- let number = Math.floor(Math.random() * 100)+1;
+const number = Math.floor(Math.random() * 100);
 
  btn.addEventListener('click', function(){
-     let input = Number(document.getElementById('userInput').value);
-     if (Number.isFinite(input)){
+    const input = Number(document.getElementById('userInput').value); //Number() işlevi nesne bağımsız değişkenini nesnenin değerini temsil eden bir sayıya dönüştürür.
+     if (Number.isFinite(input)){   //Number.isFinite()Belirli bir değerin tipi olmasıdır, bu kontrol - yöntem iletilen değer sonlu sayı olup olmadığını belirler
      if(input == number){
          output.innerHTML = `You guess right, your number was ${number}`
          outputCount.innerHTML = ` Number of Attempts : ${county}`;
@@ -22,6 +22,9 @@ var county = 1
          output.innerHTML = "You guessed too high!"
          outputCount.innerHTML = `Number of Attempts : ${county}`;
          county++;
+     }
+     if(input > 100 ){
+        output.innerHTML = "Enter a number of 100 or less."
      }
  }
  else{
